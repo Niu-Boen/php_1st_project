6e4dab3d6b191463981c81906e45de1f3289418c
Niu Boen <3435393692@qq.com>
2025-08-18 14:27:48 +0700

    first

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+<?php
+
+    echo "Hello, World!";
+    echo "<br>";
+    echo "How are you?";
+    echo "<br>";
+    echo "I'm fine";
+    echo "<br>";
+    echo 'i\'m fine';
+?>
+<?php
+
+    $x = 5;
+    $_isAdmin = true;
+    $y = 2.45;
+    $name - "Niu Boen";
+    $array = [4, 5, 0, 9];
+    
+    $GLOBALS;
+    $_SESSION;
+    $_GET;
+    $_REQUEST;
+
+    echo $x;
+    echo "<br>";
+    echo "$x anything $y";
+
+    define("PI", 3.14);
+    echo "<br>";
+    echo PI;
+    echo "<br>";
+?>
